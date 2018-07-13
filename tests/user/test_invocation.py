b8ad53729baa19be9746b9d7c8537b3c97ee2e99
Curtis La Graff <curtis@lagraff.me>
2018-07-13 19:27:28 -0400

    add respective marks to unit and user tests
    Signed-off-by: Curtis La Graff <curtis@lagraff.me>

--- tests/user/test_invocation.py
+++ tests/user/test_invocation.py
@@ -1,6 +1,8 @@
 """Test user cases via running the wordly script."""
+import pytest
 
 
+@pytest.mark.user
 def test_placeholder():
     """Placeholder test to satisfy CI until real tests are added."""
     assert True
