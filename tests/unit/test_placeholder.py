bdf835d10b9a74940d369f6fd1061cf9f3e0e72a
Curtis La Graff <curtis@lagraff.me>
2018-07-13 19:27:28 -0400

    add respective marks to unit and user tests
    Signed-off-by: Curtis La Graff <curtis@lagraff.me>

--- tests/unit/test_placeholder.py
+++ tests/unit/test_placeholder.py
@@ -1,6 +1,8 @@
 """Test placeholder until real tests are added."""
+import pytest
 
 
+@pytest.mark.unit
 def test_placeholder():
     """Placeholder test to satisfy CI until real tests are added."""
     assert True
