06725deec4d9cfae76c892c79443f6c043b93669
Curtis La Graff <curtis@lagraff.me>
2018-07-14 15:02:06 -0400

    remove placeholder test file
    Signed-off-by: Curtis La Graff <curtis@lagraff.me>

--- tests/unit/test_placeholder.py
+++
@@ -1,8 +0,0 @@
-"""Test placeholder until real tests are added."""
-import pytest
-
-
-@pytest.mark.unit
-def test_placeholder():
-    """Placeholder test to satisfy CI until real tests are added."""
-    assert True
